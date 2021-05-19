4fe2e1eda90656caa96054788d3361f8168499ae
dashixiong <876337269@qq.com>
2021-05-19 17:25:56 +0800

    fix: delete package-lock & modify package.json

--- packages/cli/.eslintrc.js
+++
@@ -1,8 +0,0 @@
-module.exports = {
-extends: ${packageName}/eslintrc.${projectType}.js,@winfe/eslint-config-winex/eslintrc.typescript-vue.js',
-extends: ${packageName}/eslintrc.${projectType}.js,@winfe/eslint-config-winex/eslintrc.typescript-vue.js',
-extends: ${packageName}/eslintrc.${projectType}.js,@winfe/eslint-config-winex/eslintrc.typescript-vue.js',
-extends: ${packageName}/eslintrc.${projectType}.js,@winfe/eslint-config-winex/eslintrc.typescript-vue.js',
-    "extends": ${packageName}/eslintrc.${projectType}.js,@winfe/eslint-config-winex/eslintrc.typescript-vue.js',
-    "root": true
-};
