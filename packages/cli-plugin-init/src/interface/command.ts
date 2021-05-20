0d0ba3d58a3b621149ea992fdcf4788d32ac097c
goodhome <565149480@qq.com>
2021-05-20 10:45:26 +0800

    feat: 删除部分存在shortcut定义的command option的类型声明

--- packages/cli-plugin-init/src/interface/command.ts
+++ packages/cli-plugin-init/src/interface/command.ts
@@ -1,16 +1,11 @@
 export interface ICommandOptions {
   name: string;
-  n: string;
   type: string;
   domain: string;
   qiankun: string;
-  qk: string;
   template: string;
-  t: string;
   repository: string;
-  r: string;
   registry: string;
   packageManager: string;
-  pm: string;
   path: string;
 }
