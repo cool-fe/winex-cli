a01bb7aa8430aaf1634cd947a548058dc7d802ff
dashixiong <876337269@qq.com>
2021-05-21 09:36:34 +0800

    fix(dependence): @winfe/get-materials替换@winexmaterials/get-materials

--- packages/cli-plugin-add/src/utils/GetMaterial.ts
+++ packages/cli-plugin-add/src/utils/GetMaterial.ts
@@ -1,27 +1,35 @@
-import chalk from 'chalk';
-import { GetMaterialOptions } from '../interface';
-import { error } from './logger';
+import chalk from "chalk";
+import { GetMaterialOptions } from "../interface";
+import { error } from "./logger";
 
-const semver = require('semver');
-const { getmaterialinfo } = require('@winexmaterials/get-materials');
+const semver = require("semver");
+const { getmaterialinfo } = require("@winfe/get-materials");
 
 class PackageNotFoundError extends Error {
   constructor(pluginName: string) {
-    super(`Package ${chalk.cyan(pluginName)} could not be found, please check the spelling right`);
-    this.name = 'PackageNotFoundError';
+    super(
+      `Package ${chalk.cyan(
+        pluginName
+      )} could not be found, please check the spelling right`
+    );
+    this.name = "PackageNotFoundError";
   }
 }
 
 class RegularNotFoundError extends Error {
   constructor(pluginName: string, version: string) {
-    super(`Couldn't find any versions for ${chalk.cyan(pluginName)} that matches ${chalk.cyan(version)}`);
-    this.name = 'RegularNotFoundError';
+    super(
+      `Couldn't find any versions for ${chalk.cyan(
+        pluginName
+      )} that matches ${chalk.cyan(version)}`
+    );
+    this.name = "RegularNotFoundError";
   }
 }
 
 export class GetMaterial {
-  pluginName: string
-  pluginVersion: string
+  pluginName: string;
+  pluginVersion: string;
 
   constructor(pluginName: string, pluginVersion: string) {
     this.pluginName = pluginName;
@@ -39,8 +47,9 @@ export class GetMaterial {
 
     let { version, type, npm, registry } = source;
 
-    if (type !== 'npm' && !!this.pluginVersion) {
-      if (!semver.valid(this.pluginVersion)) { // todo: download：不支持 dist-tags
+    if (type !== "npm" && !!this.pluginVersion) {
+      if (!semver.valid(this.pluginVersion)) {
+        // todo: download：不支持 dist-tags
         throw new RegularNotFoundError(this.pluginName, this.pluginVersion);
       } else {
         version = this.pluginVersion;
@@ -48,7 +57,7 @@ export class GetMaterial {
     }
 
     // scoped packages
-    const name = npm[0] === '@' ? npm.slice(npm.indexOf('/') + 1) : npm;
+    const name = npm[0] === "@" ? npm.slice(npm.indexOf("/") + 1) : npm;
 
     const params = {
       npm,
@@ -58,8 +67,12 @@ export class GetMaterial {
     };
 
     for (const key in params) {
-      if (params[key] === '') {
-        error(`${chalk.cyan(this.pluginName)}: lack of ${chalk.cyan(key)}, please contact the developers`);
+      if (params[key] === "") {
+        error(
+          `${chalk.cyan(this.pluginName)}: lack of ${chalk.cyan(
+            key
+          )}, please contact the developers`
+        );
         process.exit(1);
       }
     }
