162cbbbcf24fd684ac2a5e1f0156123b2982fc9d
dashixiong <876337269@qq.com>
2021-05-24 17:25:31 +0800

    fix(22): 删除package.json中的lint-staged配置

--- packages/cli-plugin-lint/src/ci.ts
+++ packages/cli-plugin-lint/src/ci.ts
@@ -23,29 +23,29 @@ const installOraInstance = ora("install");
  * 配置 package.json
  * @param eslintPath eslint检查路径
  */
-function configPackage(eslintPath: string) {
+function mdifyConfigPackage(eslintPath: string) {
   const packagePath = `${process.cwd()}/package.json`;
   const packageExist = fileUtil.checkExist(packagePath, false);
   if (packageExist) {
     // 如果package.json存在，进行修改
     const fileContent = fs.readFileSync(packagePath, "utf-8");
     const fileJSON = JSON.parse(fileContent);
-    // 增加 precommit hook
-    fileJSON.scripts = _.assign(fileJSON.scripts || {}, {
-      precommit: "lint-staged",
-    });
+    // // 增加 precommit hook
+    // fileJSON.scripts = _.assign(fileJSON.scripts || {}, {
+    //   precommit: "lint-staged",
+    // });
     // 配置 lint-staged
-    fileJSON["lint-staged"] = {
-      [eslintPath]: "eslint",
-    };
+    // fileJSON["lint-staged"] = {
+    //   [eslintPath]: "eslint",
+    // };
+
+    if (fileJSON["lint-staged"]) {
+      delete fileJSON["lint-staged"];
+    }
+
     const fileNewContent = JSON.stringify(fileJSON, null, 2);
     // 写入
     fs.writeFileSync(packagePath, fileNewContent);
-    Logger.info(
-      chalk.yellow(
-        `当前lint文件为"${eslintPath}",可根据项目具体情况调整(见package.json)`
-      )
-    );
   } else {
     // 如果 package.json 不存在，初始化失败，提示用户进行 npm 初始化
     Logger.error(
@@ -101,25 +101,33 @@ async function initLintstaged(
     installOraInstance.succeed(dep + "@" + huskyCiDeps[dep]);
   }
 
+  //init lint-staged config
+  const suffix = ["js"];
+  projectType === "vue" && suffix.push("vue");
+  supportTypeScript && suffix.push("ts");
+  const lintScript = suffix.length > 1 ? `{${suffix.join(",")}}` : suffix[0];
+  const eslintPath = `**/*.${lintScript}`;
+
   const LINT_STAGED_CONFIGJSON = {
-    "**/*.{js,ts,vue}": [
+    [eslintPath]: [
       "prettier   -c  --write  --config ./.prettierrc.js",
       "eslint  --config ./.eslintrc.js --fix",
     ],
   };
 
-  //init lint-staged config
   fs.writeFileSync(
     LINT_STAGED_CONFIG_PATH,
     `module.exports = ${JSON.stringify(LINT_STAGED_CONFIGJSON, null, 2)}`
   );
 
-  const suffix = ["js"];
-  projectType === "vue" && suffix.push("vue");
-  supportTypeScript && suffix.push("ts");
-  const lintScript = suffix.length > 1 ? `{${suffix.join(",")}}` : suffix[0];
-  const eslintPath = `*.${lintScript}`;
-  configPackage(eslintPath);
+  // 需要删除旧的lint-staged配置
+  mdifyConfigPackage(eslintPath);
+
+  Logger.info(
+    chalk.green(
+      `当前lint文件为"${LINT_STAGED_CONFIG_PATH}",可根据项目具体情况调整`
+    )
+  );
 
   Logger.info(
     chalk.yellow(`\n👏 lint-staged 配置完成, please check for sure. \n`)
