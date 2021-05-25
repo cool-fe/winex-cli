8bacaa4fff8fe01bfa84779f46dcf8953c3411f5
goodhome <565149480@qq.com>
2021-05-25 08:37:51 +0800

    feat: 取消cli中对cli version的日志打印, 后续移至cli中统一处理

--- packages/cli-plugin-init/src/utils/clearConsole.ts
+++ packages/cli-plugin-init/src/utils/clearConsole.ts
@@ -1,11 +1,4 @@
 import readline from 'readline';
-import chalk from 'chalk';
-
-const getCLIVersion = () => {
-  const localVersion = require(`../../../cli/package.json`).version;
-
-  return localVersion;
-};
 
 /**
  * 清空控制台
@@ -17,11 +10,5 @@ export const clearConsole = () => {
 
     readline.cursorTo(process.stdout, 0, 0);
     readline.clearScreenDown(process.stdout);
-
-    const version = getCLIVersion();
-
-    if (version) {
-      console.log(chalk.bold.blue(`Winex CLI v${version}\n`));
-    }
   }
 };
