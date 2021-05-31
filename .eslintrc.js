d84b71c1896734cb0d323405af7006fd4d04f12a
dashixiong <876337269@qq.com>
2021-05-31 22:19:07 +0800

    feat: add lint config

---
+++ .eslintrc.js
@@ -0,0 +1,25 @@
+module.exports = {
+  env: {
+    es6: true
+  },
+  extends: [
+    '@winfe/eslint-config-winex/eslintrc.node.js',
+    '@winfe/eslint-config-winex/eslintrc.typescript-node.js',
+    'prettier'
+  ],
+  globals: {
+    use: true
+  },
+  ignorePatterns: ['node_modules', 'dist', 'fixtures', '**/*/*.md'],
+  parserOptions: {
+    ecmaVersion: 2017
+  },
+  rules: {
+    complexity: [
+      'warn',
+      {
+        max: 10
+      }
+    ]
+  }
+}
---
+++ .lintstagedrc.js
@@ -0,0 +1,6 @@
+module.exports = {
+  '**/*.{js,ts}': [
+    'prettier   -c  --write  --config ./.prettierrc.js',
+    'eslint  --config  ./.eslintrc.js --fix'
+  ]
+}
