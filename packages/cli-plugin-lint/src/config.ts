578b46f02e8b892034eb71278200f5b7d936faf4
dashixiong <876337269@qq.com>
2021-06-03 13:50:09 +0800

    fix: 升级@winfe/plugin-winfe

--- packages/cli-plugin-lint/src/config.ts
+++ packages/cli-plugin-lint/src/config.ts
@@ -7,51 +7,51 @@ interface ProjectDepConfig {
 }
 
 export const commonDeps: DepConfig = {
-  eslint: "7.25.0",
-  "@babel/eslint-parser": "7.13.14",
-  "babel-eslint": "10.1.0",
-};
+  eslint: '7.25.0',
+  '@babel/eslint-parser': '7.13.14',
+  'babel-eslint': '10.1.0'
+}
 
 export const tsDeps: DepConfig = {
-  typescript: "4.2.4",
-  "@typescript-eslint/parser": "4.22.1",
-  "@typescript-eslint/eslint-plugin": "4.22.1",
-  "eslint-plugin-typescript": "0.14.0",
-};
+  typescript: '4.2.4',
+  '@typescript-eslint/parser': '4.22.1',
+  '@typescript-eslint/eslint-plugin': '4.22.1',
+  'eslint-plugin-typescript': '0.14.0'
+}
 
 export const configDeps: ProjectDepConfig = {
   default: {
-    "eslint-config-airbnb-base": "14.2.1",
-    "eslint-plugin-import": "2.22.1",
-  },
-};
+    'eslint-config-airbnb-base': '14.2.1',
+    'eslint-plugin-import': '2.22.1'
+  }
+}
 
 export const pluginDeps: ProjectDepConfig = {
   vue: {
-    "eslint-plugin-vue": "7.9.0",
-    "eslint-plugin-html": "6.1.2",
+    'eslint-plugin-vue': '7.9.0',
+    'eslint-plugin-html': '6.1.2'
   },
   node: {
-    "eslint-plugin-node": "11.1.0",
-  },
-};
+    'eslint-plugin-node': '11.1.0'
+  }
+}
 
 export const DeafultSharedEslintConfig: DepConfig = {
-  "@winfe/eslint-config-winex": "0.0.6",
-};
+  '@winfe/eslint-config-winex': '0.0.7'
+}
 
-export const winfeCiDeps: DepConfig = {};
+export const winfeCiDeps: DepConfig = {}
 
 export const huskyCiDeps: DepConfig = {
-  husky: "6.0.0",
-};
+  husky: '6.0.0'
+}
 
 export const lintstagedCiDeps: DepConfig = {
-  "lint-staged": "11.0.0",
-};
+  'lint-staged': '11.0.0'
+}
 
 export const prettierDeps: DepConfig = {
-  prettier: "2.3.0",
-  "eslint-config-prettier": "8.3.0",
-  "eslint-plugin-prettier": "3.4.0",
-};
+  prettier: '2.3.0',
+  'eslint-config-prettier': '8.3.0',
+  'eslint-plugin-prettier': '3.4.0'
+}
--- packages/cli-plugin-lint/src/index.ts
+++ packages/cli-plugin-lint/src/index.ts
@@ -1,105 +1,104 @@
-import { BasePlugin } from "@winfe/cli-core";
-import { createCliPrompt } from "./prompts";
-import { installDeps } from "./deps-install";
-import configEslintRC from "./install-config";
-import configPrettierRC from "./add-prettier-config";
-import configEditorrRC from "./add-editor-config";
-import interEslintToCI from "./ci";
-import chalk from "chalk";
-import { Logger } from "../lib/logger";
+import { BasePlugin } from '@winfe/cli-core'
+import chalk from 'chalk'
+import { createCliPrompt } from './prompts'
+import { installDeps } from './deps-install'
+import configEslintRC from './install-config'
+import configPrettierRC from './add-prettier-config'
+import configEditorrRC from './add-editor-config'
+import interEslintToCI from './ci'
+import { Logger } from '../lib/logger'
 
 export enum HookEngine {
-  none = "none",
-  husky = "husky",
+  none = 'none',
+  husky = 'husky',
   // yorkie = "yorkie",
-  winfe = "winfe",
+  winfe = 'winfe'
 }
 
 export type PluginOptions = {
-  env: "browser" | "node";
+  env: 'browser' | 'node';
   typescript: boolean;
-  pm: "yarn" | "npm";
+  pm: 'yarn' | 'npm';
   hookEngine?: HookEngine;
   ci?: boolean;
-};
+}
 
 export default class LintPlugin extends BasePlugin {
   commands = {
     lint: {
-      usage: "init or upgrade eslint prettier editorconfig ",
-      lifecycleEvents: ["init"],
+      usage: 'init or upgrade eslint prettier editorconfig ',
+      lifecycleEvents: ['init'],
       options: {
-        "--env [browser]": {
-          usage: "代码运行环境：node、browser",
-          config: {},
+        '--env [browser]': {
+          usage: '代码运行环境：node、browser',
+          config: {}
         },
-        "--typescript": {
-          usage: "是否支持typescript",
+        '--typescript': {
+          usage: '是否支持typescript'
         },
-        "--pm [pm]": {
-          usage: "包管理器",
+        '--pm [pm]': {
+          usage: '包管理器',
           config: {
-            default: "yarn",
-          },
+            default: 'yarn'
+          }
         },
-        "--hook-engine [husky]": {
-          usage: "注册git hook的引擎，husky、yorkie、git-hooks、winfe 等",
+        '--hook-engine [husky]': {
+          usage: '注册git hook的引擎，husky、yorkie、git-hooks、winfe 等',
           config: {
-            default: "husky",
-          },
+            default: 'husky'
+          }
         },
-        "--ci": {
-          usage: "是否在ci/cd中",
+        '--ci': {
+          usage: '是否在ci/cd中'
         },
-        "--verbose": {
-          usage: "是否显示详细的日志信息",
-        },
-      },
-    },
-  };
+        '--verbose': {
+          usage: '是否显示详细的日志信息'
+        }
+      }
+    }
+  }
 
-  answer: object = {};
+  answer: object = {}
 
   hooks = {
-    "before:lint:init": async (content: any) => {
+    'before:lint:init': async (content: any) => {
       // 安装相关依赖
       const {
         env: _env,
         typescript: _ts,
         pm: _pm,
-        ci,
-      } = content?.parsedOptions?.options as PluginOptions;
-      if (ci) return;
+        ci
+      } = content?.parsedOptions?.options as PluginOptions
+      if (ci) return
       const anwser = await createCliPrompt<PluginOptions>({
         env: _env,
         typescript: _ts,
-        pm: _pm,
-      });
-      this.answer = anwser;
-      await installDeps(anwser);
+        pm: _pm
+      })
+      this.answer = anwser
+      await installDeps(anwser)
     },
-    "lint:init": async (content: any) => {
+    'lint:init': async (content: any) => {
       // 配置eslint
-      const { env, typescript, pm: _pm } = this?.answer as PluginOptions;
-      await configEslintRC(env === "node" ? "node" : "vue", typescript);
+      const { env, typescript, pm: _pm } = this?.answer as PluginOptions
+      await configEslintRC(env === 'node' ? 'node' : 'vue', typescript)
       // 配置prettier
-      await configPrettierRC(_pm);
+      await configPrettierRC(_pm)
       // 配置editorconfig
-      await configEditorrRC();
+      await configEditorrRC()
     },
-    "after:lint:init": async (content: any) => {
+    'after:lint:init': async (content: any) => {
       // 修改package.json，结合--hook-engine对eslint、prettier、editorconfig做设置
-      const { hookEngine = HookEngine["husky"] } = content?.parsedOptions
-        ?.options as PluginOptions;
-      const { env, typescript, pm } = this?.answer as PluginOptions;
-      await interEslintToCI(hookEngine, env, typescript, pm);
+      const { hookEngine = HookEngine.husky } = content?.parsedOptions?.options as PluginOptions
+      const { env, typescript, pm } = this?.answer as PluginOptions
+      await interEslintToCI(hookEngine, env, typescript, pm)
 
       Logger.info(
-        chalk`\n🎉{bold Successfully configured eslint, prettier, editorconfig in your project}\n`
-      );
+        chalk`\n🎉{bold Successfully configured eslint, prettier, editorconfig in your project. happy coding~}\n`
+      )
 
-      Logger.info(chalk`\t{bold To get started： }`);
-      Logger.info(chalk`\t{bold Reload the  editor & experience}\n`);
-    },
-  };
+      Logger.info(chalk`\t{bold To get started： }`)
+      Logger.info(chalk`\t{bold Reload the  editor & experience}\n`)
+    }
+  }
 }
