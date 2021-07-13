f46d8ea4a8a6d9c020d9ef38f506e727d057c5b0
huzhiwei <865953771@qq.com>
2021-07-13 11:11:30 +0800

    fix: 修改fire build打包报错

--- packages/cli-plugin-fire/src/build/index.ts
+++ packages/cli-plugin-fire/src/build/index.ts
@@ -3,26 +3,30 @@ import chalk from 'chalk';
 import CreateWebpackCompoConfig from './webpack.component';
 
 export default (dir = [process.cwd()]): Promise<void> =>
-  new Promise<void>((resolve, reject) => {
-    webpack(CreateWebpackCompoConfig(dir), (err, stats) => {
-      if (!err) {
-        console.log(chalk.green('webpack build success'));
-        resolve();
-        if (stats) {
-          process.stdout.write(
-            stats.toString({
-              colors: true,
-              modules: false,
-              children: false,
-              chunks: false,
-              chunkModules: false
-            })
-          );
+  new Promise((resolve, reject) => {
+    CreateWebpackCompoConfig(dir).forEach(conf => {
+      webpack(conf, (err, stats) => {
+        if (!err) {
+          console.log(chalk.green('webpack build success'));
+          if (stats) {
+            process.stdout.write(
+              stats.toString({
+                colors: true,
+                modules: false,
+                children: false,
+                chunks: false,
+                chunkModules: false
+              })
+            );
+          }
+          resolve();
+        } else {
+          console.log('err:', err);
+          console.log(err.stack && err.stack);
+          reject(err);
         }
-      } else {
-        console.log('err:', err);
-        console.log(err.stack && err.stack);
-        reject(err);
-      }
-    });
-  });
+      });
+    })
+  })
+  
+  
