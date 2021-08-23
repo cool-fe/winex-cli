8b74f4e0e12a9c80e7b659644cfa5492c7fc0d07
huzhiwei <865953771@qq.com>
2021-08-23 14:00:05 +0800

    feat(build): 修改打包配置

--- packages/cli-plugin-fire/src/upload.ts
+++ packages/cli-plugin-fire/src/upload.ts
@@ -1,14 +1,10 @@
-/* eslint-disable import/no-dynamic-require */
-/* eslint-disable @typescript-eslint/ban-ts-comment */
-import Minio from 'minio';
 import path from 'path';
 import chalk from 'chalk';
 import fs from 'fs';
 
-// eslint-disable-next-line @typescript-eslint/no-var-requires
-const pkg = require(`${process.cwd()}/package.json`);
+const Minio = require('minio');
 
-module.exports = async () => {
+export default async () => {
   const minioClient = new Minio.Client({
     endPoint: '172.16.6.51',
     port: 9000,
@@ -16,44 +12,45 @@ module.exports = async () => {
     accessKey: 'www.winning.com.cn',
     secretKey: 'www.winning.com.cn'
   });
-  const filepath = `materials/${pkg.name}/${pkg.version}/`;
-  const file = `${process.cwd()}/lib/`;
-  // const filename = pkg.name.split("/")[1] || "";
+  const pkg = require(`${process.cwd()}/package.json`);
   const metaData = {
     'Content-Type': 'application/octet-stream',
     'X-Amz-Meta-Testing': 1234,
     example: 5678
   };
 
-  // 上传方法
-  const upload = (_path: string, _file: string) => {
-    minioClient.fPutObject('winex', _path, _file, metaData, (err) => {
-      if (!err) {
-        console.log(chalk.green(`上传成功`));
-      } else {
-        console.log(err, 'err');
-      }
-    });
-  };
-
   // 区分文件和文件夹然后上传
-  const classifyFiles = (__path: string, __file: string) => {
-    fs.readdir(__file, (err, files) => {
+  // const materialClassify = pkg.name.split('/')[0].split('-')[1]
+  const domainName = pkg.name.split('/')[1].split('-')[0];
+  const filepath = `materials-umd-lib/${domainName}/${pkg.name}/${pkg.version}/`;
+  const file = `${process.cwd()}/lib/`;
+
+  const classifyFiles = (filepath: string, file: any) => {
+    fs.readdir(file, (err, files) => {
       files.forEach((filename) => {
         const filedir = path.join(file, filename);
-        fs.stat(filedir, (error, stats) => {
-          if (!error) {
+        fs.stat(filedir, (err, stats) => {
+          if (!err) {
             const isFile = stats.isFile(); //是文件
             if (isFile) {
-              upload(`${__path}${filename}`, `${__file}${filename}`);
+              upload(`${filepath}${filename}`, `${file}${filename}`);
             } else {
-              classifyFiles(`${__path}${filename}/`, `${__file}${filename}/`);
+              classifyFiles(`${filepath}${filename}/`, `${file}${filename}/`);
             }
           }
         });
       });
     });
   };
-
+  // 上传方法
+  const upload = (filepath: string, files: any) => {
+    minioClient.fPutObject('winex', filepath, files, metaData, (err: any) => {
+      if (!err) {
+        console.log(chalk.green(`upload success`));
+      } else {
+        console.log(err, 'err');
+      }
+    });
+  };
   classifyFiles(filepath, file);
 };
