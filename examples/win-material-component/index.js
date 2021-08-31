84983ca452fc752643fb25688d15b7a7eaf4835a
dashixiong <876337269@qq.com>
2021-08-31 13:34:37 +0800

    build: upgrade vmi@1.2.6

--- examples/win-material-component/index.js
+++ examples/win-material-component/index.js
@@ -1,4 +1,5 @@
 import encounterFilter from './src/win-encounter-filter.vue';
+import './index.scss';
 
 encounterFilter.install = function(Vue) {
   // Vue.component(encounterFilter.name, encounterFilter);
